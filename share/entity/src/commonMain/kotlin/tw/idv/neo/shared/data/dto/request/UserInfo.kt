b576bf09ef978172f3dc3b54b917ba7ca2c0498d
Neo <c.min.tseng@gmail.com>
2023-08-12 02:45:02 +0800

    rename class & add typealias

--- share/entity/src/commonMain/kotlin/tw/idv/neo/shared/data/dto/request/UserInfo.kt
+++ share/entity/src/commonMain/kotlin/tw/idv/neo/shared/data/dto/request/UserInfo.kt
@@ -3,7 +3,11 @@ package tw.idv.neo.shared.data.dto.request
 import kotlinx.serialization.*
 
 @Serializable
-data class RegisterInfo(
+data class UserInfo(
     val username: String,
     val password: String
 )
+
+//Ref : https://juejin.cn/post/6844904184911757325
+typealias RegisterInfo = UserInfo
+typealias LoginInfo = UserInfo
